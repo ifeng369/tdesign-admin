3a99dea53210424a1bfcca324aa9848ad6e92330
易峰 <wsyifeng@163.com>
2024-10-07 09:29:03 +0800

    123

--- src/config/style.ts
+++ src/config/style.ts
@@ -1,16 +1,31 @@
 export default {
+  // 显示 底部
   showFooter: true,
+  // 收缩侧边栏
   isSidebarCompact: false,
+  //显示 Breadcrumbs
   showBreadcrumb: false,
+  // 菜单自动折叠
   menuAutoCollapsed: false,
+  //明亮模式 可选值 dark 暗色  light 亮色 auto 跟随系统
   mode: "light",
+  // 布局 可选值 side 侧边栏  top 顶部  mix 混合
   layout: "side",
+  // 分割菜单
   splitMenu: false,
+  // 侧边栏主题 可选值 dark 暗色  light 亮色
   sideMode: "light",
-  isFooterAside: false,
+
+  //固定侧边栏
   isSidebarFixed: true,
+  // 固定头部
   isHeaderFixed: true,
+
+  //使用 多标签Tab页
   isUseTabsRouter: false,
-  // showHeader: true,
+
+  //显示 Header
+  showHeader: true,
+  // 主题颜色
   brandTheme: "#0052D9",
 };
