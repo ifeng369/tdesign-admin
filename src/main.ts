57d5513b10df6ffb65932c8287b493333628c057
易峰 <wsyifeng@163.com>
2024-10-08 11:23:13 +0800

    12131

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 /* eslint-disable simple-import-sort/imports */
 import { createApp } from "vue";
-import TDesign from "tdesign-vue-next";
+// import TDesign from "tdesign-vue-next";
 
 import App from "./App.vue";
 import router from "./router";
@@ -12,7 +12,7 @@ import "@/style/index.less";
 
 const app = createApp(App);
 
-app.use(TDesign);
+// app.use(TDesign);
 app.use(store);
 app.use(router);
 
